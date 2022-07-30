7164deb1f3c44cf3d3df6338cfada3e7d5dbe328
mohmad dorqi <mohmad.dorqi4748@gmail.com>
2022-07-30 10:52:50 +0430

    validation input

--- src/Components/validate.js
+++ src/Components/validate.js
@@ -1,7 +1,28 @@
 const validate =(data)=>{
-
-
-    if(!data.name){
-        return 'name reqered'
+    const errors={}
+    const regexEmail=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+    const regexPassword=/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
+    if(!data.name.trim()){
+      errors.name="Username required"
+    }else{
+            delete errors.name
     }
+
+    if(!data.email){
+        errors.email="Email required"
+    }else if(!data.email.test(regexEmail)){
+
+        errors.email="Invalid Email"
+        
+    }else{
+        delete errors.email
+    }
+    if(!data.password){
+       errors.password="Password required"
+    }else if(!data.password.test(regexPassword)){
+        errors.password="Password Should at last 8 charecter "
+    }else{
+      delete  errors.password
+    }
+    
 }
